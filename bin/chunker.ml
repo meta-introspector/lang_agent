4ca4775bbd4e29cd58355798af8a93d55ceb11bb
mike dupont <mike.dupont@introspector.local>
2024-01-18 10:46:17 -0500

    building

--- bin/chunker.ml
+++ bin/chunker.ml
@@ -1,6 +1,6 @@
 open Lang_agent    
 
-let run () =
+let run (prompt) =
   let model = "mistral" in
   let client = Ollama.create_client model in
   ignore
@@ -9,8 +9,20 @@ let run () =
        Ollama.(
          send
            client
-           ~prompt: "Tell me an epic story about the epic battle between the AI llamas and the functional ocaml camels"
+           ~prompt:  prompt
            ())
        (Lwt_io.printlf "res: %s")
 
-let () = run ()
+
+let read_file  =
+  let filename  = Sys.argv.(1) in
+  let chan = open_in filename in
+  try
+    while true; do
+      let l1 = input_line chan in 
+      run l1
+    done;
+  with End_of_file ->
+    close_in chan
+
+let () = read_file 
