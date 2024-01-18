2f3164a1a315eac03fd6c085b6484465e6adbaf0
mike dupont <mike.dupont@introspector.local>
2024-01-18 11:12:35 -0500

    prompt working

--- bin/chunker.ml
+++ bin/chunker.ml
@@ -4,7 +4,7 @@ let run (prompt) =
   (print_endline (
       "#+begin_src input\n" ^
       prompt ^
-      "#+end_src input"));
+      "\n#+end_src input"));
   
   let model = "mistral" in
   let client = Ollama.create_client model in
@@ -19,16 +19,24 @@ let run (prompt) =
        (Lwt_io.printlf "#+begin_src output\n%s#+end_src output")
 
 
+let read_whole_file =
+  let filename  = Sys.argv.(1) in
+  let ch = open_in_bin filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
 
 let read_file  =
-  let filename  = Sys.argv.(1) in
+  let prompt = read_whole_file in
+  let filename  = Sys.argv.(2) in
   let chan = open_in filename in
   try
     while true; do
       let l1 = input_line chan in 
-      run l1
+      run (prompt ^ l1)
     done;
   with End_of_file ->
     close_in chan
 
+    
 let () = read_file 
