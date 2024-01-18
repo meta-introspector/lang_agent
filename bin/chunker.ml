ce6fbda1cd6747401e94edfa370cf6c6fa3660d1
mike dupont <mike.dupont@introspector.local>
2024-01-18 12:03:43 -0500

    adding separator

--- bin/chunker.ml
+++ bin/chunker.ml
@@ -2,9 +2,9 @@ open Lang_agent
 
 let run (prompt) =
   (print_endline (
-      "#+begin_src input\n" ^
+      "\n#+begin_src input\n" ^
       prompt ^
-      "\n#+end_src input"));
+      "\n#+end_src input\n"));
   
   let model = "mistral" in
   let client = Ollama.create_client model in
@@ -16,27 +16,36 @@ let run (prompt) =
            client
            ~prompt:  prompt
            ())
-       (Lwt_io.printlf "#+begin_src output\n%s#+end_src output")
+       (Lwt_io.printlf "\n#+begin_src output\n%s\n#+end_src output")
 
 
-let read_whole_file =
+let read_prompt_file =
   let filename  = Sys.argv.(1) in
+  let ch = open_in_bin filename in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
+  
+let read_sep_file =
+  let filename  = Sys.argv.(2) in
   let ch = open_in_bin filename in
   let s = really_input_string ch (in_channel_length ch) in
   close_in ch;
   s
 
 let read_file  =
-  let prompt = read_whole_file in
-  let filename  = Sys.argv.(2) in
+  let prompt = read_prompt_file in
+  let sep = read_sep_file in  
+  let filename  = Sys.argv.(3) in
   let chan = open_in filename in
   try
     while true; do
       let l1 = input_line chan in 
-      run (prompt ^ l1)
+      run (prompt ^ l1 ^ sep )
     done;
   with End_of_file ->
     close_in chan
+
 
     
 let () = read_file 
