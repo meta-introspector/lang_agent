e6ad3b4ffcaad3ae23009c6736ef6a29f449ec30
mike dupont <mike.dupont@introspector.local>
2024-02-04 12:57:30 -0500

    progress

--- bin/scanner.ml
+++ bin/scanner.ml
@@ -9,7 +9,6 @@ let last_line_matching ic : string =
     ! line
   with End_of_file ->
     close_in ic;
-    (print_endline ("DEBUG4:" ^  !line));
     !line
 
     (* let result = if Str.string_match pattern line 0 then Some line else None in *)
@@ -22,12 +21,49 @@ let last_line_matching ic : string =
    let pair_pattern = Str.regexp "([^:]+):\\([0-9]+\\)"
   
  *)
+(*coq/lib/loc.ml *)
+(* type source = *)
+(*   (\* OCaml won't allow using DirPath.t in InFile *\) *)
+(*   | InFile of { dirpath : string option; file : string } *)
 
+(* type location = { *)
+(*     lc_fname : source; (\** filename or toplevel input *\) *)
+(*     lc_line_nb : int; (\** start line number *\) *)
+(*     ln_bol_pos : int; (\** position of the beginning of start line *\) *)
+(*     ln_line_nb_last : int; (\** end line number *\) *)
+(*     ln_bol_pos_last : int; (\** position of the beginning of end line *\) *)
+(*     ln_bp : int; (\** start position *\) *)
+(*     ln_ep : int; (\** end position *\) *)
+(* } *)
 
+(* type location_list = location list *)
 
+let rec parse_loc_list a : string =
+  match a with
+  | [] -> ""
+  | head :: rest -> head ^ "," ^ parse_loc_list rest
+
+let parse_loc (line :string)  =
+  let parts2 = String.split_on_char ':' line in
+  let fname = parse_loc_list parts2  in
+  (print_endline ("DEBUG5:" ^ fname));
+  fname
+     (* { *)
+     (*   lc_fname = fname; (\** filename or toplevel input *\) *)
+     (*   lc_line_nb = lineno; (\** start line number *\) *)
+     (* } *)
+
+let rec process_fp l  =
+  match l with
+  | h :: t -> parse_loc h :: process_fp t
+  | [] -> []
+
+let parse_file_points (line: string)  =
+  let parts1 = String.split_on_char ',' line in
+  process_fp parts1
 
 (* A function that traverses a directory and prints the last line matching the pair pattern for each file *)
-let traverse_and_print path =
+let traverse_and_print path  =
   (print_endline ("DEBUG0:" ^  path));
   let rec aux dir =
     let entries = Sys.readdir dir in
@@ -43,19 +79,22 @@ let traverse_and_print path =
           (*Unix.file_kind  *)
           let fs = Unix.lstat full_path in
           match fs.st_kind with
-          | S_CHR -> (print_endline ("DEBUG2 char" ^  full_path));
-          | S_SOCK -> (print_endline ("DEBUG2 char"^  full_path));
-          | S_BLK -> (print_endline ("DEBUG2 block"^  full_path));
-          | S_FIFO -> (print_endline ("DEBUG2 FIFO"^  full_path));
-          | S_LNK -> (print_endline ("DEBUG2 LINK"^  full_path));
+          | S_CHR -> (print_endline ("DEBUG2 char" ^  full_path)); 
+          | S_SOCK -> (print_endline ("DEBUG2 char"^  full_path)); 
+          | S_BLK -> (print_endline ("DEBUG2 block"^  full_path)); 
+          | S_FIFO -> (print_endline ("DEBUG2 FIFO"^  full_path)); 
+          | S_LNK -> (print_endline ("DEBUG2 LINK"^  full_path)); 
           | S_DIR -> (print_endline ("DEBUG2 DIR" ^  full_path));
           | S_REG ->
              if Sys.file_exists  full_path then
                (* (print_endline ("DEBUG2 " ^  full_path)); *)
              let ic = open_in full_path in
-             let ll =  last_line_matching ic
-             in
-             (print_endline ("DEBUG2 " ^  full_path  ^ " LL " ^ ll));
+             let ll =  last_line_matching ic in
+             print_endline ("DEBUG2 " ^  String.concat "@" (parse_file_points ll))
+             (*
+               (print_endline ("DEBUG2 " ^  full_path  ^ " LL " ^ ll));
+               fp
+              *)
                 
       ) entries
   in
@@ -69,11 +108,16 @@ let anon_fun filename =
 let () =
 
   let start = ref "" in
+  let window_size = ref 128 in  
   let help_str = "test" in
   let opts = [
-    "-s", Arg.Set_string start, "startdir";
+      "-s", Arg.Set_string start, "startdir";
+      "-w", Arg.Set_int window_size, "window_size";
   ] |> Arg.align in
 
   Arg.parse opts anon_fun help_str;
   Printf.printf "DEBUG %s\n" !start;
-  traverse_and_print !start
+  traverse_and_print !start 
+  (*
+    map_lookup_file_snippets fp snippet_size
+   *)
