bb2a03a2bdceece1b3f9972ea4324f57b8331a30
mike dupont <mike.dupont@introspector.local>
2024-02-12 10:39:34 -0500

    run example

---
+++ run_example3.sh
@@ -0,0 +1,9 @@
+while true;
+do 
+dune exec bin/scanner.exe -- -c .txt -x .spec  \
+    -s  ./huggingface/metacoq/common/batch1/folder67/ \
+    -p "Take this proof, treat as spec or a workflow for orchestration of many llm agents. Show that each variable in the proof maps into the workflow. Show that the workflow matches the steps of the proof. execute workflow to generate a sample data, generate a grammar to constrain the output, generate a function to test the output and prove its correct and then feed back into the original proof. .:"  \
+    --openai -m "mixtral" -u "https://dev-hub.agentartificial.com"
+
+    
+done;
