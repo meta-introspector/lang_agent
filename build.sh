bab80e573ff3a0f5d4924b1d6bbfafb0d7af5cbd
mike dupont <mike.dupont@introspector.local>
2024-02-12 10:47:16 -0500

    change order

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 sudo apt install -y opam
 opam init --shell-setup -y
 eval $(opam env --switch=default)
-opam install --yes --deps-only .
- opam install --yes ppx_yojson_conv_lib
-
  opam repo add coq-released https://coq.inria.fr/opam/released
  opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
+
+
+ opam install --yes ppx_yojson_conv_lib
+
  opam install --yes coq
  opam install --yes ppx_yojson lwt_ppx containers lambdasoup uri ezcurl ppx_yojson_conv ppx_yojson_conv_lib ezcurl-lwt yojson
+opam install --yes --deps-only .
  dune build .
