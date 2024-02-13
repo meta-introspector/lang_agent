41a34f072072056b7a2ea6a21274722ac98710a1
mike dupont <mike.dupont@introspector.local>
2024-02-13 02:17:30 -0500

    smaller build

---
+++ build2.sh
@@ -0,0 +1,12 @@
+set -e
+
+#eval $(opam env --switch=default)
+#opam switch 
+eval $(opam env --switch=5.1.2+trunk)
+ opam install --yes ppx_yojson_conv_lib
+
+
+ opam install --yes ppx_yojson lwt_ppx containers lambdasoup uri ezcurl ppx_yojson_conv ppx_yojson_conv_lib ezcurl-lwt yojson
+ opam install --yes --deps-only .
+ opam pin add --yes openai https://github.com/meta-introspector/openai-ocaml.git#main --update-invariant
+ dune build .
