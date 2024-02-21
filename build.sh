347ddfe0e81a6bb99694e9865ac0a066913523f3
mike dupont <mike.dupont@introspector.local>
2024-02-21 10:46:51 -0500

    splitup

--- build.sh
+++ build.sh
@@ -2,15 +2,5 @@ set -e
 sudo apt update
 sudo apt install -y opam libgmp-dev libcurl4-gnutls-dev
 opam init --shell-setup -y
-eval $(opam env --switch=default)
- opam repo add coq-released https://coq.inria.fr/opam/released
- opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
 
-
- opam install --yes ppx_yojson_conv_lib stdio
-
- opam install --yes coq
- opam install --yes ppx_yojson lwt_ppx containers lambdasoup uri ezcurl ppx_yojson_conv ppx_yojson_conv_lib ezcurl-lwt yojson
- opam install --yes --deps-only .
- opam pin add --yes openai https://github.com/meta-introspector/openai-ocaml.git#main
- dune build .
+source ./build2.sh
--- build2.sh
+++ build2.sh
@@ -1,13 +1,14 @@
 set -e
 
-#eval $(opam env --switch=default)
-#opam switch 
-#eval $(opam env --switch=5.1.2+trunk)
 eval $(opam env --switch=4.1.13)
+ opam repo add coq-released https://coq.inria.fr/opam/released
+ opam repo add coq-extra-dev https://coq.inria.fr/opam/extra-dev
+
  opam install --yes ppx_yojson_conv_lib
-
-
  opam install --yes ppx_yojson lwt_ppx containers lambdasoup uri ezcurl ppx_yojson_conv ppx_yojson_conv_lib ezcurl-lwt yojson stdio
  opam install -w --yes --deps-only .
  opam pin add --yes openai https://github.com/meta-introspector/openai-ocaml.git#main --update-invariant
  dune build .
+ echo the following are optional and may take a long time
+ opam install --yes coq
+  opam install --yes serlib
