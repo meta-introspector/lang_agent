5df21a770a7c33c4ca3f61124887c73d5468c20f
mike dupont <mike.dupont@introspector.local>
2024-02-25 18:33:06 -0500

    adding openai dep

--- flake.nix
+++ flake.nix
@@ -1,11 +1,17 @@
 {
   inputs = {
+  openai.owner = "meta-introspector";
+    openai.repo = "openai-ocaml";
+    openai.rev = "da27c7644812ac6faed480b8a295830f920fcd3e";
+    openai.type = "github";
+	  
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";	
     opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.follows = "opam-nix/nixpkgs";
+    
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+  outputs = { self, flake-utils, opam-nix, nixpkgs, openai }@inputs:
     # Don't forget to put the package name instead of `throw':
     let package = "lang_agent";
     in flake-utils.lib.eachDefaultSystem (system:
@@ -13,7 +19,7 @@
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
         scope =
-          on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
+          on.buildDuneProject { } package ./. { ocaml-base-compiler = "*"; };
         overlay = final: prev:
           {
             # Your overrides go here
