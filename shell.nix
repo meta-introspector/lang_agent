148272b87609bbe2aa0d13d649c389a0efad9022
mike dupont <mike.dupont@introspector.local>
2024-02-25 17:40:53 -0500

    nix

--- shell.nix
+++ shell.nix
@@ -14,3 +14,15 @@ pkgs.mkShellNoCC {
 ;; nixpkgs.emacsPackages.proofgeneral
 ;; nixpkgs.emacsPackages.magit
 ;; nixpkgs.emacsPackages.straight_HEAD
+
+with import <nixpkgs> {};
+let
+  ocaml = ocaml-ng.ocamlPackages_4_13.ocaml;
+  opam2nix = import ./opam2nix.nix {};
+  selection = opam2nix.build {
+    inherit ocaml;
+    selection = ./opam-selection.nix;
+    src = ./.;
+  };
+in
+selection.lang_agent
