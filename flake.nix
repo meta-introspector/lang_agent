c59cf08881676055bd37a55a74c30646cff73549
mike dupont <mike.dupont@introspector.local>
2024-02-25 19:06:28 -0500

    git

--- flake.nix
+++ flake.nix
@@ -1,10 +1,18 @@
 {
   inputs = {
-  openai.owner = "meta-introspector";
-    openai.repo = "openai-ocaml";
-    openai.rev = "da27c7644812ac6faed480b8a295830f920fcd3e";
-    openai.type = "github";
-	  
+#     openai = {
+#     owner = "meta-introspector";
+#     repo = "openai-ocaml";
+#     rev = "da27c7644812ac6faed480b8a295830f920fcd3e";
+#     type = "github";
+# #    opamInputs = 
+# #      {
+# #        inherit ppx_yojson_conv ppx_yojson ocaml lwt_ppx
+# #        ezcurl-lwt dune conf-openssl;
+# #      };
+# #      opamSrc = "openai.opam";
+#     };
+    openai.url = "github:meta-introspector/openai-ocaml/0.0.2";	
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";	
     opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
@@ -16,10 +24,11 @@
     let package = "lang_agent";
     in flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+#        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; overlays = [ openai ]; };
         on = opam-nix.lib.${system};
         scope =
-          on.buildDuneProject { } package ./. { ocaml-base-compiler = "*"; };
+          on.buildDuneProject { } package  ./. { ocaml-base-compiler = "*"; };
         overlay = final: prev:
           {
             # Your overrides go here
